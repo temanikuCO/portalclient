b99c1fd498bda497392684e2f087fcaa3cbc097f
temanikuCO <ruciandreas@gmail.com>
2026-08-13 09:51:54 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyCMhQHE6scBOAx3_rre0fq7pzf_qgV4EA8",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "6285176869464" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
